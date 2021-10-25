c8b6aa369452fde712fe1ff48f7768268c33fb2a
thanhthuan12 <dothanhthuan0202@gmail.com>
2021-10-25 11:01:58 +0700

    lần đẩy thứ hai

---
+++ src/dothanhthuan2005110006K14DCMT01/studentTestDriver.java
@@ -0,0 +1,36 @@
+package dothanhthuan2005110006K14DCMT01;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class studentTestDriver {
+    public static void main(String[] args) {
+        // tạo 1 danh sách sing viên 
+        student[] studentList = new student[2];
+
+        // nhập danh sách sinh viên 
+        Scanner varInput = new Scanner(System.in);
+
+        for(int i = 0; i < studentlist.length;  i++){
+            System.out.println("Nhập sinh viên thứ" + (i + 1));
+            System.out.println("Tên sinh viên: ");
+            string name = varInput.nextLine();//15 ENTER
+            studentList[i] = new student(name, age);
+        }   
+
+        // in danh sách sinh viên 
+        System.out.println("==========in sách sinh viên sau
+        nhập==========");
+        for (student student : studentList) {
+            student.inThonhTin();
+
+        }
+
+        list list = Arrays.asList(a)
+        
+        
+    }
+        
+    }
+    
+}
